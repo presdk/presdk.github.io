fa668f8517a9bc26bb3bba0f51468eee6bf5bace
presdk <dawinkweon@gmail.com>
2020-05-04 21:05:03 +1200

    improve(style): give Noto Sans JP font for headers and Roboto for text for easier readability

--- src/App.js
+++ src/App.js
@@ -17,11 +17,21 @@ import {
 } from "react-router-dom";
 import BlogPage from "./pages/BlogPage";
 import Fade from "./animations/Fade";
+import styled from "styled-components";
 
-const DefaultStyle = {
-  fontFamily: "Roboto', sans-serif",
-  fontSize: "1em",
-};
+const StyledApp = styled.div`
+  * {
+    font-family: "Roboto", sans-serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    font-family: "Noto Sans JP", sans-serif;
+  }
+`;
 
 const store = createStore(
   rootReducer,
@@ -32,52 +42,54 @@ const App = () => {
   return (
     <Router>
       <Provider store={store}>
-        <Container className="mt-4 px-4" style={DefaultStyle}>
-          <Row className="sticky-top">
-            <Col className="d-xs-block d-sm-none col-xs-12 mb-3">
-              <MainHeader />
-            </Col>
-          </Row>
-          <Row>
-            <Col className="d-md-block d-none col-md-3 mx-auto text-center">
-              <Fade className="sticky-top">
-                <div className="mb-3">
-                  <MainHeader />
-                </div>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Link to="/gallery">Gallery</Link>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <Link as="li" to="/blog">
-                      Blog
-                    </Link>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Fade>
-            </Col>
-            <Col xs={12} md={9}>
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={() => <Redirect to="/gallery" />}
-                />
-                <Route path="/gallery">
-                  <PicturePostPage />
-                </Route>
-                <Route path="/blog">
-                  <BlogPage />
-                </Route>
-              </Switch>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <FixedFooter className="fixed-bottom" />
-            </Col>
-          </Row>
-        </Container>
+        <StyledApp>
+          <Container className="mt-4 px-4">
+            <Row className="sticky-top">
+              <Col className="d-xs-block d-sm-none col-xs-12 mb-3">
+                <MainHeader />
+              </Col>
+            </Row>
+            <Row>
+              <Col className="d-md-block d-none col-md-3 mx-auto text-center">
+                <Fade className="sticky-top">
+                  <div className="mb-3">
+                    <MainHeader />
+                  </div>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item>
+                      <Link to="/gallery">Gallery</Link>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <Link as="li" to="/blog">
+                        Blog
+                      </Link>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Fade>
+              </Col>
+              <Col xs={12} md={9}>
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    render={() => <Redirect to="/gallery" />}
+                  />
+                  <Route path="/gallery">
+                    <PicturePostPage />
+                  </Route>
+                  <Route path="/blog">
+                    <BlogPage />
+                  </Route>
+                </Switch>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <FixedFooter className="fixed-bottom" />
+              </Col>
+            </Row>
+          </Container>
+        </StyledApp>
       </Provider>
     </Router>
   );
