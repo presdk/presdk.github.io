c09a9845f9b8b57279df5e11875709632721621b
Dawin Kweon <dawin.kweon@eroad.com>
2021-04-12 21:02:55 +1200

    Updates

---
+++ precache-manifest.2afdb432f8aefc2b815fc544198ff4ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0d686f7bfc307114e3027cd5b800f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1466ee7b1f153baac5d2",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b47ce4cebfd21cd43f1f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1466ee7b1f153baac5d2",
+    "url": "/static/js/2.7b0bbf27.chunk.js"
+  },
+  {
+    "revision": "411dec2b61903e3259a7af36bdcc1a45",
+    "url": "/static/js/2.7b0bbf27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47ce4cebfd21cd43f1f",
+    "url": "/static/js/main.51962592.chunk.js"
+  },
+  {
+    "revision": "d8cb6c628b13220d5fae",
+    "url": "/static/js/runtime-main.d11fe4ef.js"
+  },
+  {
+    "revision": "d151896963fe62e64f45e0741c911fc0",
+    "url": "/static/media/avatar.d1518969.jpg"
+  }
+]);
